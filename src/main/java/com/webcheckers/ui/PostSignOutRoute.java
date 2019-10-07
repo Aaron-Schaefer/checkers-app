3a030cbed866dbd76071ad9914013bb42addf57a
An Chang (Mark) <mc7377@rit.edu>
2019-10-07 11:00:21 -0400

    Goodluck in my fellow Dawgs and Cats

---
+++ src/main/java/com/webcheckers/ui/PostSignOutRoute.java
@@ -0,0 +1,18 @@
+package com.webcheckers.ui;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Session;
+import spark.TemplateEngine;
+import static spark.Spark.halt;
+
+public class PostSignOutRoute implements Route {
+
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return null;
+    }
+}
