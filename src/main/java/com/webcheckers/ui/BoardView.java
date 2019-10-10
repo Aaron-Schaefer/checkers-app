5030423a5e4dca146166bfb0f77ec5b9173149d9
Ries Scerbin <rs7804@rit.edu>
2019-10-10 15:44:44 -0400

    Commented Board view

--- src/main/java/com/webcheckers/ui/BoardView.java
+++ src/main/java/com/webcheckers/ui/BoardView.java
@@ -20,7 +20,11 @@ public class BoardView implements Iterable {
     private Player redPlayer;
     private Player whitePlayer;
 
-
+    /**
+     *Constructor for the boardview
+     * @param board
+     * @param player
+     */
     public BoardView(Board board, Player player){
         rows = new ArrayList<Row>();
 
@@ -39,8 +43,18 @@ public class BoardView implements Iterable {
         }
     }
 
+    /**
+     *Gets the active color (the one that currently being moved)
+     * @return the color
+     */
     public Piece.Color getActiveColor(){return activeColor;}
 
+    /**
+     * Gets the players color based on the piece they are playing
+     * @param player
+     * @param model
+     * @return the players piece color
+     */
     private Piece.Color getPlayerColor(Player player, Board model) {
         if (model.getWhitePlayer().equals(player) ) {
             return Piece.Color.WHITE;
@@ -51,11 +65,22 @@ public class BoardView implements Iterable {
         return null;
     }
 
-
+    /**
+     * A method to get the boards white player
+     * @return the white player
+     */
     public Player getWhitePlayer(){return whitePlayer;}
 
+    /**
+     * A method to get the boards red player
+     * @return the red player
+     */
     public Player getRedPlayer(){return redPlayer;}
 
+    /**
+     *Get the current active player (the one whose move it is currently)
+     * @return the player
+     */
     public Player getActivePlayer(){
         if (activeColor == Piece.Color.RED){
             return redPlayer;
@@ -63,6 +88,11 @@ public class BoardView implements Iterable {
         else
             return whitePlayer;
     }
+
+    /**
+     *Creates a java iterable object for the row
+     * @return the iterator
+     */
     @Override
     public Iterator<Row> iterator() {
         return rows.iterator();
