aff20176577c42f04f64e66bf8d1a887ad742dda
gb9951 <gb9951@g.rit.edu>
2019-11-16 15:25:53 -0500

    Commented and added more Routes to the WebServer. Added some functionality to the GetReplayStopWatchingRoute.

--- src/main/java/com/webcheckers/ui/PostReplayPreviousTurnRoute.java
+++ src/main/java/com/webcheckers/ui/PostReplayPreviousTurnRoute.java
@@ -1,4 +1,48 @@
 package com.webcheckers.ui;
 
-public class PostReplayPreviousTurnRoute {
+import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.TemplateEngine;
+
+import java.util.Objects;
+import java.util.logging.Logger;
+
+public class PostReplayPreviousTurnRoute implements Route {
+
+    private static final Logger LOG = Logger.getLogger(PostReplayPreviousTurnRoute.class.getName());
+
+    private final TemplateEngine templateEngine;
+
+    private Gson gson;
+
+    /**
+     * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
+     *
+     * @param templateEngine
+     *   the HTML template rendering engine
+     */
+    public PostReplayPreviousTurnRoute(final TemplateEngine templateEngine, Gson gson) {
+        this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+        this.gson = gson;
+        //
+        LOG.config("PostReplayPreviousTurnRoute is initialized.");
+    }
+
+
+    /**
+     * Render the WebCheckers SignIn page.
+     *
+     * @param request
+     *   the HTTP request
+     * @param response
+     *   the HTTP response
+     *
+     * @return
+     *   the rendered HTML for the Home page
+     */
+    public Object handle(Request request, Response response) {
+        return null;
+    }
 }
