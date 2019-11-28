ee53380f0729cda4e8b2c0cc5e82847ff284d8b1
An Chang (Mark) <mc7377@rit.edu>
2019-11-27 21:42:43 -0500

    BoardView test and Game test finished

--- src/test/java/com/webcheckers/model/BoardViewTest.java
+++ src/test/java/com/webcheckers/model/BoardViewTest.java
@@ -1,24 +1,35 @@
 package com.webcheckers.model;
 
-import com.webcheckers.appl.PlayerLobby;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.mock;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Class to test the BoardView
+ *
+ * @author An Chang (Mark).
+ */
 
 @Tag("Model-tier")
 public class BoardViewTest {
 
+    /**
+     * Fields used to test board view.
+     */
     private BoardView CuT1;
     private BoardView CuT2;
     private Board board;
     private Player redPlayer;
     private Player whitePlayer;
 
+    /**
+     * Setup before each test.
+     */
     @BeforeEach
-    private void setup(){
+    private void Setup(){
 
         redPlayer = mock(Player.class);
         whitePlayer = mock(Player.class);
@@ -28,6 +39,9 @@ public class BoardViewTest {
 
     }
 
+    /**
+     * Test the player retrival.
+     */
     @Test
     private void testPlayerRetrival(){
 
@@ -36,5 +50,6 @@ public class BoardViewTest {
 
 
     }
+
 
 }
---
+++ src/test/java/com/webcheckers/model/GameTest.java
@@ -0,0 +1,72 @@
+package com.webcheckers.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Class to test the Game
+ *
+ * @author An Chang (Mark).
+ */
+
+@Tag("Model-tier")
+public class GameTest {
+
+    /**
+     * Fields used to test the Game class.
+     */
+    private Game CuT;
+    private Board board;
+    private Player redPlayer;
+    private Player whitePlayer;
+
+    /**
+     * Setup before each test.
+     */
+    @BeforeEach
+    private void Setup(){
+
+        redPlayer = mock(Player.class);
+        whitePlayer = mock(Player.class);
+        board = mock(Board.class);
+        CuT = new Game(redPlayer, whitePlayer,1);
+
+    }
+
+    /**
+     * Test if players are added properly.
+     */
+    @Test
+    private void testPlayerAddition(){
+
+        assertEquals(CuT.getRedPlayer(),redPlayer);
+        assertEquals(CuT.getWhitePlayer(),whitePlayer);
+
+    }
+
+    /**
+     * Test if the board can be retrieved properly.
+     */
+    @Test
+    private void testGetBoard(){
+
+        assertEquals(CuT.getBoard(),board);
+
+    }
+
+    /**
+     * Test if the game ID can be retrieved properly.
+     */
+    @Test
+    private void getGameID(){
+
+        assertEquals(CuT.getGameID(),1);
+
+    }
+
+
+}
